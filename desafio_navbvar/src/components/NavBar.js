64d4a53e8e304eca31a2ceb26a2636e73b80b87f
darwinJ <djmendoza100@gmail.com>
2021-11-01 22:33:50 -0500

    desafio navbar

--- desafio_navbvar/src/components/NavBar.js
+++ desafio_navbvar/src/components/NavBar.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import '../App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
+//import logoSitio from '..components/logoTienda.png';
+//creando el navbar
 function NavBar(){
     return(
-        <div className="NavBar">
+        <React.Fragment>
+            <div className="miHeader">
 
-            <header ClassName="miEstiloHeader">
-                <a ClassName="estiloMi-Logo" href="#">CeluPc</a>
-                <nav>
-                    <ul>
-                        <li><a href="#">Inicio</a></li>
-                        <li><a href="#">Productos</a></li>
-                        <li><a href="#">Contactanos</a></li>
-                        <li><a href="#">Registrate</a></li>
-                        <li><a href="#">Login</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+                <div className="contenedor_menu">
+                    {/* <img className="mi_Estilo_imagen" src={logoSitio} alt="imagen principal logo" /> */}
+                    <a href="#" className="miEstiloLogo">MovilPc</a>
+                    <nav className="miNavbar">
+                        <ul>
+                            <li><a className="miEstiloMenu" href="#">Inicio</a></li>
+                            <li><a className="miEstiloMenu" href="#">PC</a></li>
+                            <li><a className="miEstiloMenu" href="#">Reloj</a></li>
+                            <li><a className="miEstiloMenu" href="#">SmartPhone</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+            </div>
+        </React.Fragment>
+        
     );
 }
+
 export default NavBar;
